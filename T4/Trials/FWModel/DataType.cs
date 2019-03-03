0aa11efd3637f21bf8c890de8f18cec401d1b717
Rohith Venkat <rohithv97@gmail.com>
2019-03-03 21:20:59 +0530

    Add files via upload

---
+++ T4/Trials/FWModel/DataType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    public class IntType
+    {
+
+    }
+    public class NameType
+    {
+
+    }
+    public class MailIDType
+    {
+
+    }
+    public class ContactNumberType
+    {
+
+    }
+}
---
+++ T4/Trials/FWModel/EntityTypes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTypes;
+using EnumTypes;
+
+namespace EntityFactory
+{
+    public class Attribute
+    {
+        public string Name { get; set; }
+        public DataTypeValues dataType { get; set; }
+        public string Label { get; set; }
+    }
+
+    public class Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        List<Attribute> x = new List<Attribute>();
+        public List<Attribute> Attributes
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+    }
+
+    public class EntityCollections
+    {
+        List<Entity> ec = new List<Entity>();
+        public List<Entity> Entities
+        {
+            get { return ec; }
+            set { ec = value; }
+        }
+    }
+
+}
